4f62bd2f44a95c9d33540446bc93b4cb375e5837
RC <rc.chandru@gmail.com>
2017-08-20 22:58:40 +0530

    Update to use latest release

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Board } from "react-trello";
 
@@ -23,8 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Trello Demo</h2>
+          <h3>React Trello Demo</h3>
         </div>
         <p className="App-intro">
           <Board data={data} draggable
