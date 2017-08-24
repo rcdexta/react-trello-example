b93ab8cefc2b385ffc21364e403c7931306d0e3a
RC <rc.chandru@gmail.com>
2017-08-24 05:58:34 -0400

    Update to latest package. Add more features to demonstrate

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
-import React, { Component } from "react";
-import "./App.css";
-import { Board } from "react-trello";
+import React, {Component} from 'react'
+import './App.css'
+import {Board} from 'react-trello'
 
-const data = require("./data.json");
+const data = require('./data.json')
 
 const handleDragStart = (cardId, laneId) => {
-  console.log("drag started");
-  console.log(`cardId: ${cardId}`);
-  console.log(`laneId: ${laneId}`);
-};
-
-const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
-  console.log("drag ended");
-  console.log(`cardId: ${cardId}`);
-  console.log(`sourceLaneId: ${sourceLaneId}`);
-  console.log(`targetLaneId: ${targetLaneId}`);
-};
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h3>React Trello Demo</h3>
-        </div>
-        <p className="App-intro">
-          <Board data={data} draggable
-            handleDragStart={handleDragStart}
-            handleDragEnd={handleDragEnd}/>
-        </p>
-      </div>
-    );
-  }
+    console.log('drag started')
+    console.log(`cardId: ${cardId}`)
+    console.log(`laneId: ${laneId}`)
 }
 
-export default App;
+const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
+    console.log('drag ended')
+    console.log(`cardId: ${cardId}`)
+    console.log(`sourceLaneId: ${sourceLaneId}`)
+    console.log(`targetLaneId: ${targetLaneId}`)
+}
+
+class App extends Component {
+
+    setEventBus = eventBus => {
+        this.setState({eventBus})
+    }
+
+    completeCard = () => {
+      this.state.eventBus.publish({type: 'ADD_CARD', laneId: 'COMPLETED', card: {id: "Milk", title: "Buy Milk", label: "15 mins", description: "Use Headspace app"}})
+      this.state.eventBus.publish({type: 'REMOVE_CARD', laneId: 'PLANNED', cardId: "Milk"})
+    }
+
+    addCard = () => {
+      this.state.eventBus.publish({type: 'ADD_CARD', laneId: 'BLOCKED', card: {id: "Ec2Error", title: "EC2 Instance Down", label: "30 mins", description: "Main EC2 instance down"}})
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h3>React Trello Demo</h3>
+                </div>
+                <div className="App-intro">
+                    <button onClick={this.completeCard} style={{margin: 5}}>
+                        Complete Buy Milk
+                    </button>
+                    <button onClick={this.addCard} style={{margin: 5}}>
+                        Add Blocked
+                    </button>
+                    <Board
+                        data={data}
+                        draggable
+                        eventBusHandle={this.setEventBus}
+                        handleDragStart={handleDragStart}
+                        handleDragEnd={handleDragEnd}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App
